a75b4ee296c608bd4e51ba0b9ef6e2f76b63319b
Sonoely Rivera <srivera@student.fullsail.edu>
2024-07-09 23:50:16 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Memory Lane Assignment`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
