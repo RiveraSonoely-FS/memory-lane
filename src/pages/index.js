5eb8e8d9d5fc806fb58005c8ded0a750b5f26d5c
Sonoely Rivera <srivera@student.fullsail.edu>
2024-07-19 16:58:16 -0400

    add images & new article.

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,17 +57,28 @@ const HomePage = () => {
             knowing that overcoming these challenges will prepare me for a rewarding career in web development. With dedication and a 
             passion for learning, I am committed to achieving my goal of graduating and contributing to the future of digital innovation.
           </p>
+          <br/>
+          <img src="https://blog.postman.com/wp-content/uploads/2023/03/230404-Blog-Diagram.jpg" style={pictureStyles} />
         <h3>My Experience Building an API - Jul 14</h3>
           <p>
             Building an API from scratch was a challenging yet rewarding experience. It taught me how to connect the dots between front-end 
             and back-end development. I gained hands-on experience in handling databases, implementing CRUD operations, and managing data flow.
-
             This journey also reinforced the importance of clear documentation and robust error handling, which are crucial for any developer 
-            working with APIs. As I move forward in my studies, I feel more confident tackling complex projects and contributing to real-world applications.
-
+            working with APIs. <br/>
+            As I move forward in my studies, I feel more confident tackling complex projects and contributing to real-world applications.
             In conclusion, building an API has been a foundational experience in my web development education. It has provided me with skills and knowledge 
             that will serve me well in my future career. If you’re a fellow student or just starting your coding journey, I encourage you to dive into API 
             development—it's an invaluable skill that will enhance your understanding of web applications.
+          </p>
+          <br/>
+          <img src="https://www.winklix.com/blog/wp-content/uploads/2019/02/all-about-react-native-apps-776x415.png" style={pictureStyles} />
+        <h3>Exploring React Native - Jul 19</h3>
+          <p>
+          The React Native framework seamlessly extends my JavaScript skills into the realm of mobile app development, a familiar environment with powerful 
+          capabilities. What strikes me most is its component-based architecture, which mirrors React.js and simplifies the creation of modular, reusable code. 
+          The supportive community and comprehensive documentation have been incredibly helpful in providing guidance and solutions whenever I encounter challenges. 
+          React Native is not just a tool; it's a pathway to mastering cross-platform mobile development, empowering me to build innovative applications efficiently 
+          and effectively.
           </p>
       </div>
       <footer style={footerStyles}>
